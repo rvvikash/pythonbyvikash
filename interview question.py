b2b6cf914e77b595f31397adee0c239ba945538b
rvvikash <38788306+rvvikash@users.noreply.github.com>
2024-06-12 17:26:53 +0530

    Create interview question.py

---
+++ interview question.py
@@ -0,0 +1,22 @@
+-- max from list --- 
+list=[1,4,35,9,0,45,1,8]
+s=0
+for i in list:
+    if s<i:
+        s=i
+print(s)
+
+merge in panda:
+
+merged_df = pd.merge(df1, df2, on='key', how='inner')
+
+increase the salary 10%
+
+UPDATE table_name
+SET salary = salary * 1.10
+WHERE salary < 10000;
+
+# Input = "aaccbbcccdaaaaddde"
+# #Output: a2c2b2c3d1a4d3e1
+
+sort the list in python
